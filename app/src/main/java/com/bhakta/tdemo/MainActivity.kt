e35531513be340382ef11edeeae19d212b411e97
Bhakta Ranjan Satapathy <sr.bhakta108@gmail.com>
2020-12-11 15:07:47 -0800

    Initial Commit

---
+++ app/src/main/java/com/bhakta/tdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bhakta.tdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/bhakta/tdemo/remote/FeedsApiService.kt
@@ -0,0 +1,4 @@
+package com.bhakta.tdemo.remote
+
+class FeedsApiService {
+}
